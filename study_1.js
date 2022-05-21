8ba45e10c9b88828060325774c021e9f96e8914c
unknown <bwaburi007@gmail.com>
2022-05-21 10:03:27 +0300

    outputting strings & math operations

---
+++ study_1.js
@@ -0,0 +1,11 @@
+// outputting a message or string
+console.log("Hello World!");
+
+//addition, subtraction, multiplication, division
+console.log(5 + 5);
+console.log(10 - 3);
+console.log(5 * 5);
+console.log(50 / 5);
+
+//string concatenation
+console.log("My name is " + "Brian");
