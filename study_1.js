4986948683570bc0b6d701ea6bab5a19542f752b
unknown <bwaburi007@gmail.com>
2022-05-21 11:24:50 +0300

    using template literals

--- study_1.js
+++ study_1.js
@@ -17,13 +17,15 @@ console.log(myName);
 
 //variable names can be changed without using 'let' again
 myName = "Tracy";
-<<<<<<< HEAD
 console.log(myName);
 
 //constant
 //constants cannot be changed as opposed to variables
 const vehicleBrand = "Toyota";
 console.log("The vehicle you selected is a " + vehicleBrand);
-=======
-console.log(myName);
->>>>>>> 5de250e3e5364bba9952c8ea91655e704ce1d163
+
+//template literals
+const country = "Malaysia";
+
+//entire string is enclosed with backticks
+console.log(`Hello ${myName} and welcome to ${country}`);
